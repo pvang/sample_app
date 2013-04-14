f3cdcba4ddd23cb5d5bd37f698e16403fed049be
PaNgia Vang <pangia.v@gmail.com>
2013-04-14 03:35:00 -0500

    Exercise 3.5.2 (RSpec let Function)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
@@ -9,8 +11,11 @@ describe "Static pages" do
 		end
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
+			# page.should have_selector('title',
+						# :text => "Ruby on Rails Tutorial Sample App | Home")
 			page.should have_selector('title',
-						:text => "Ruby on Rails Tutorial Sample App | Home")
+						:text => "#{base_title} | Home")
+
 		end
 	end
 
@@ -21,8 +26,10 @@ describe "Static pages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
+			# page.should have_selector('title',
+						# :text => "Ruby on Rails Tutorial Sample App | Help")
 			page.should have_selector('title',
-						:text => "Ruby on Rails Tutorial Sample App | Help")
+						:text => "#{base_title} | Help")
 		end
 	end
 
@@ -33,8 +40,10 @@ describe "Static pages" do
 		end
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
+			# page.should have_selector('title',
+						# :text => "Ruby on Rails Tutorial Sample App | About Us")
 			page.should have_selector('title',
-						:text => "Ruby on Rails Tutorial Sample App | About Us")
+						:text => "#{base_title} | About Us")
 		end
 	end
 
@@ -45,9 +54,10 @@ describe "Static pages" do
 		end
 		it "should have the title 'Contact'" do
 			visit '/static_pages/contact'
+			# page.should have_selector('title',
+						# :text => "Ruby on Rails Tutorial Sample App | Contact")
 			page.should have_selector('title',
-						# :text => "#{base_title} | Contact")
-						:text => "Ruby on Rails Tutorial Sample App | Contact")
+						:text => "#{base_title} | Contact")
 		end
 	end
 
